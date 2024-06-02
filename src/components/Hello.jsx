ce1cba40d9e2dcfc0f56630d58dcd8995911cd9c
jowel007 <abuhanifjowel1@gmail.com>
2024-06-02 14:31:29 +0600

    jsx explain

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,5 +1,14 @@
+
+// const name = "robi";
+
+// function displayMessage(){
+//     return "WOW !";
+// }
+
 function Hello(){
-    return <h1>Hello From Component !</h1>
+    return <div>
+        <h1>Hello From Component ! </h1> <h1>wow</h1>
+    </div>
 }
 
 export default Hello
